e245be2ebc9199618a2c7e0b18526092b3925768
Rexford Essilfie <rexfordessilfie09@gmail.com>
2023-04-04 03:02:43 -0500

    move test folder to top level for clean dist

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,9 @@ require("@ariesclark/eslint-config/eslint-patch");
 
 module.exports = {
 	root: true,
-	extends: ["@ariesclark/eslint-config"]
+	extends: ["@ariesclark/eslint-config"],
+	parserOptions: {
+		// Support eslint in `test` (i.e non-build) folders: https://typescript-eslint.io/linting/troubleshooting/#i-get-errors-telling-me-eslint-was-configured-to-run--however-that-tsconfig-does-not--none-of-those-tsconfigs-include-this-file
+		project: ["./tsconfig.json", "./tsconfig.eslint.json"]
+	}
 };
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,6 +1,6 @@
 import test from "ava";
 
-import { t } from ".";
+import { t } from "../src";
 
 test("tanu", (tc) => {
 	tc.truthy(t);
