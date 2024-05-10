6f6896f41e3abadc90ef5b326d611b18f9638ad8
Evgenii Mozharovskii <mozharovsky@live.com>
2024-05-10 06:16:48 +0400

    chore: add prettier config

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,9 @@ module.exports = {
 	extends: ["@ariesclark/eslint-config"],
 	parserOptions: {
 		// Support eslint in `test` (i.e non-build) folders: https://typescript-eslint.io/linting/troubleshooting/#i-get-errors-telling-me-eslint-was-configured-to-run--however-that-tsconfig-does-not--none-of-those-tsconfigs-include-this-file
-		project: ["./tsconfig.json", "./tsconfig.eslint.json"]
-	}
+		project: ["./tsconfig.json", "./tsconfig.eslint.json"],
+	},
+	prettier: {
+		endOfLine: "auto",
+	},
 };
---
+++ prettier.config.cjs
@@ -0,0 +1,10 @@
+/** @type {import('prettier').Config} */
+const config = {
+	tabWidth: 4,
+	useTabs: true,
+	printWidth: 120,
+	endOfLine: "auto",
+	trailingComma: "es5",
+};
+
+module.exports = config;
